21d5a6268538158944a556a958408ab84900cedb
Madhan Kumar <106460217+Madhanjetti@users.noreply.github.com>
2023-06-28 21:33:05 +0530

    Add files via upload

--- Main5.java
+++ Main5.java
@@ -35,3 +35,73 @@ public class Main
 		}
 	}
 }
+// Solid Rhombus
+public class Main
+{
+	public static void main(String[] args) {
+		int n=5;
+		int i,j;
+		for(i=1;i<=n;i++){
+		    for(j=1;j<=(n-i);j++){
+		        System.out.print(" ");
+		    }
+		    for(int k=1;k<=n;k++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
+//hollow rhombus pattern
+public class Main
+{
+	public static void main(String[] args) {
+		int n=5;
+		int i,j;
+		for(i=1;i<=n;i++){
+		    for(j=1;j<=(n-i);j++){
+		        System.out.print(" ");
+		    }
+		    for(int k=1;k<=n;k++){
+			if(k==1||j==1|j==n|k==n){
+		        System.out.print("*");
+			}
+			else{
+			    System.out.print(" ");
+			}
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
+//diamond pattern
+public class Main
+{
+	public static void main(String[] args) {
+		int n=5;
+		int i,j;
+		for(i=1;i<=n;i++){
+		    for(j=1;j<=(n-i);j++){
+		        System.out.print(" ");
+		    }
+		   for(int k=1;k<=(2*i)-1;k++){
+		       System.out.print("*");
+		   }
+		    
+		    System.out.println();
+		}
+		for(i=n;i>=1;i--){
+		    for(j=1;j<=(n-i);j++){
+		        System.out.print(" ");
+		    }
+		   for(int k=1;k<=(2*i)-1;k++){
+		       System.out.print("*");
+		   }
+		    
+		    System.out.println();
+		}
+		
+	}
+}
